6c86ade9f31b888852e799694ffa73be0e788536
Ah_shateri <74296449+AmirHosseinShateri@users.noreply.github.com>
2024-02-07 08:43:23 +0330

    Create backtracking_solve.py

---
+++ src/backtracking_solve.py
@@ -0,0 +1,29 @@
+def solve_sudoku_with_cages(sudoku_board, cage_info):
+    #todo
+        return None
+
+def recursive_backtracking(assignment, variables, domains, constraints, cage_constraints):
+    #todo
+    return None
+
+def forward_pruning(var, value, assignment, variables, domains, constraints):
+    #todo
+
+    return pruned_vars, pruned_domains
+
+def select_unassigned_variable(assignment, variables, constraints):
+    #todo
+def order_domain_values(var, assignment, variables, domains, constraints):
+    #todo
+
+def count_conflicts(var, value, assignment, variables, domains, constraints):
+    #todo
+    return count
+
+def is_consistent(var, value, assignment, constraints):
+   #todo
+
+def generate_constraints():
+    #todo
+    return constraints
+
